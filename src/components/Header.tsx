a3f1aa4445b4ce08c9f6b1b5331dad764e270b10
Dev Luca <lucahesper@hotmail.com>
2022-12-27 17:22:53 -0300

    created about me

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
-export default function Header() {
+export function Header() {
   return (
-    <div>
+    <>
       <h1 className="absolute flex items-center font-ubuntu pt-5 text-3xl text-cyan-50 before:content-['<'] before:text-cyan-400 before:text-3xl after:content-['/>'] after:text-cyan-400 after:text-3xl">LucaDestefano</h1>
       <header className="w-full flex justify-center pt-6">
         <nav className="flex items-center gap-12 text-cyan-50 text-xl">
@@ -9,6 +9,6 @@ export default function Header() {
           <a href="" className="border-b-[2.5px] border-transparent hover:text-cyan-400 hover:border-b-[2.5px] hover:translate-x-2 hover:border-cyan-300 transition" >Contato</a>
         </nav>
       </header>
-    </div>
+    </>
   )
 }
