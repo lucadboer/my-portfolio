28926ea35abf3acb8dd9beb4adb095d332a08de9
Dev Luca <lucahesper@hotmail.com>
2023-01-02 09:59:44 -0300

    add visual effect in the home

--- src/components/Homes.tsx
+++ src/components/Homes.tsx
@@ -1,6 +1,25 @@
+import React, { useEffect } from 'react'
+import Typed from 'typed.js'
 import { Header } from './Header'
 
 export function Homes() {
+  const el = React.useRef(null)
+  const typed = React.useRef(null)
+
+  useEffect(() => {
+    const options = {
+      strings: ['Desenvolvedor Front-end'],
+      typeSpeed: 65,
+      backSpeed: 50,
+    }
+
+    typed.current = new Typed(el.current, options)
+
+    return () => {
+      typed.current.destroy()
+    }
+  }, [])
+
   return (
     <div className="max-w-6xl mx-auto">
       <header>
@@ -12,11 +31,11 @@ export function Homes() {
           <strong className="block text-5xl text-cyan-300">
             Luca Destefano Boer
           </strong>
-          sou um
-          <strong className="text-4xl text-transparent bg-clip-text bg-gradient-to-r from-cyan-300 to-cyan-100">
-            {' '}
-            Desenvolvedor Front-end
-          </strong>
+          sou um{' '}
+          <strong
+            ref={el}
+            className="text-4xl text-transparent bg-clip-text bg-gradient-to-r from-cyan-300 to-cyan-100"
+          />
         </p>
       </main>
     </div>
