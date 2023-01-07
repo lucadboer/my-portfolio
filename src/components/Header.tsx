06987628bd9322b6c12dbe3bd418e98a1a8efc8a
Dev Luca <lucahesper@hotmail.com>
2023-01-07 09:09:05 -0300

    changed border mobile menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,15 +4,22 @@ import { useState } from 'react'
 
 export function Header() {
   const [openMenu, setOpenMenu] = useState(false)
-
   return (
     <>
       <h1 className="absolute flex items-center font-ubuntu pt-5 text-3xl text-cyan-50 before:content-['<'] before:text-cyan-400 before:text-3xl after:content-['/>'] after:text-cyan-400 after:text-3xl">
         <Link href="/">DevLuca</Link>
       </h1>
-      <header className="w-full md:flex justify-center max-lg:justify-end max-lg:pr-4 pt-6">
+      <header
+        id="menu"
+        onClick={() => {
+          if (openMenu) {
+            setOpenMenu(false)
+          }
+        }}
+        className="w-full md:flex justify-center max-lg:justify-end max-lg:pr-4 pt-6"
+      >
         <button
-          className="md:hidden z-20 fixed right-0 mr-5 text-white border-2 p-1 mt-[-10px] rounded-md border-cyan-400 transition hover:bg-cyan-400"
+          className="md:hidden z-50 fixed right-0 mr-5 text-white border-2 p-1 mt-[-10px] rounded-md border-cyan-400 transition hover:bg-cyan-400"
           onClick={() => setOpenMenu(!openMenu)}
         >
           {openMenu ? <X size={32} /> : <List size={32} />}
@@ -24,25 +31,25 @@ export function Header() {
         >
           <a
             href="#aboutme"
-            className="block w-max mt-4 border-b-[2.5px] border-cyan-400 tracking-wide hover:text-cyan-400 transition hover:border-b-[2.5px] hover:-translate-y-1 max-my-4"
+            className="block w-max mt-4 border-b-[1px] border-cyan-400 tracking-wide hover:text-cyan-400 transition hover:border-b-[1px] hover:-translate-y-1 max-my-4"
           >
             Sobre mim
           </a>
           <a
             href="#technologies"
-            className="block w-max mt-4 border-b-[2.5px] border-cyan-400 tracking-wide hover:text-cyan-400 transition hover:border-b-[2.5px] hover:-translate-y-1 max-my-4"
+            className="block w-max mt-4 border-b-[1px] border-cyan-400 tracking-wide hover:text-cyan-400 transition hover:border-b-[1px] hover:-translate-y-1 max-my-4"
           >
             Tecnologias
           </a>
           <a
             href="#projects"
-            className="block w-max mt-4 border-b-[2.5px] border-cyan-400 tracking-wide hover:text-cyan-400 transition hover:border-b-[2.5px] hover:-translate-y-1 max-my-4"
+            className="block w-max mt-4 border-b-[1px] border-cyan-400 tracking-wide hover:text-cyan-400 transition hover:border-b-[1px] hover:-translate-y-1 max-my-4"
           >
             Projetos
           </a>
           <a
             href="#contactMe"
-            className="block w-max mt-4 max-md:mb-4 border-b-[2.5px] border-cyan-400 tracking-wide hover:text-cyan-400 transition hover:border-b-[2.5px] hover:-translate-y-1 max-my-4"
+            className="block w-max mt-4 max-md:mb-4 border-b-[1px] border-cyan-400 tracking-wide hover:text-cyan-400 transition hover:border-b-[1px] hover:-translate-y-1 max-my-4"
           >
             Contato
           </a>
