522c15660d15278691be7f918a20ca20c0037262
Luca Destefano <lucahesper@hotmail.com>
2023-12-23 07:04:42 -0300

    adjust aboutMe

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -14,25 +14,18 @@ export function AboutMe() {
         </h2>
         <p className="text-gray-100 text-xl leading-relaxed max-lg:text-lg">
           Olá pessoa, robô ou IA, que sorte a minha ter você por aqui! Então
-          vamos lá, me chamo Luca e tenho 20 anos. Sou um Programador Fullstack
-          com foco nas stacks do Javascript, me especializando em Next.js,
-          Node.js e React Native na plataforma da{' '}
-          <Link
-            className="text-purple-600 transition hover:text-purple-500"
-            href="https://rocketseat.com.br"
-            target="_blank"
-          >
-            Rocketseat
-          </Link>{' '}
-          . Também estou cursando o último semestre de Análise e Desenvolvimento
-          de Sistemas. Sempre fui apaixonado por tecnologia desde pequenino, e o
+          vamos lá, me chamo Luca e tenho 20 anos, sou 
+          formado em Análise e Desenvolvimento de Sistemas. Atuo como Desenvolvedor Fullstack
+          com foco nas stacks do Javascript com proeficiência nas seguintes
+          tecnologias: React Native e Next.js (React), também possuo conhecimento em Node.js. Sempre fui apaixonado por tecnologia desde pequenino, e o
           que me fez brilhar os olhos mais ainda em programação foi entender que
           consigo solucionar alguns dos problemas do mundo através dos códigos e
-          da tecnologia, tornando ele um lugar melhor. E atualmente busco uma
-          oportunidade para poder atuar nesse universo pelo qual tenho muito
-          carinho e não canso de estudar sobre, afinal, provavelmente serei uma
+          da tecnologia, tornando ele um lugar melhor. Sou movido pela curiosidade e 
+          constantemente busco me aprofundar nesse universo, sempre em busca de aprender mais 
+          e me atualizar sobre as últimas tendências e avanços na área pelo qual tenho muito carinho. Afinal, provavelmente serei uma 
           das pessoas mais curiosas que você pode encontrar.
         </p>
+        {/*
         <button className="border-4 border-cyan-400 w-max text-2xl text-gray-100 rounded-lg transition hover:shadow-button hover:shadow-cyan-200 hover:-translate-y-2 shadow-xl shadow-black">
           <a
             className="flex items-center gap-4 py-3 px-4"
@@ -43,6 +36,7 @@ export function AboutMe() {
             <DownloadSimple size={28} /> Baixar CV
           </a>
         </button>
+      */}
       </div>
       <div className="max-md:hidden">
         <lottie-player
