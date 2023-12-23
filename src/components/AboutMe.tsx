2252ac4992f34dc9becd8a6a7cec178f66ad08da
Luca <luca.boer@outlook.com>
2023-12-23 07:17:43 -0300

    fix: eslint

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,6 +1,4 @@
 /* eslint-disable react/no-unescaped-entities */
-import Link from 'next/link'
-import { DownloadSimple } from 'phosphor-react'
 
 export function AboutMe() {
   return (
@@ -14,16 +12,19 @@ export function AboutMe() {
         </h2>
         <p className="text-gray-100 text-xl leading-relaxed max-lg:text-lg">
           Olá pessoa, robô ou IA, que sorte a minha ter você por aqui! Então
-          vamos lá, me chamo Luca e tenho 20 anos, sou 
-          formado em Análise e Desenvolvimento de Sistemas. Atuo como Desenvolvedor Fullstack
-          com foco nas stacks do Javascript com proeficiência nas seguintes
-          tecnologias: React Native e Next.js (React), também possuo conhecimento em Node.js. Sempre fui apaixonado por tecnologia desde pequenino, e o
-          que me fez brilhar os olhos mais ainda em programação foi entender que
-          consigo solucionar alguns dos problemas do mundo através dos códigos e
-          da tecnologia, tornando ele um lugar melhor. Sou movido pela curiosidade e 
-          constantemente busco me aprofundar nesse universo, sempre em busca de aprender mais 
-          e me atualizar sobre as últimas tendências e avanços na área pelo qual tenho muito carinho. Afinal, provavelmente serei uma 
-          das pessoas mais curiosas que você pode encontrar.
+          vamos lá, me chamo Luca e tenho 20 anos, sou formado em Análise e
+          Desenvolvimento de Sistemas. Atuo como Desenvolvedor Fullstack com
+          foco nas stacks do Javascript com proeficiência nas seguintes
+          tecnologias: React Native e Next.js (React), também possuo
+          conhecimento em Node.js. Sempre fui apaixonado por tecnologia desde
+          pequenino, e o que me fez brilhar os olhos mais ainda em programação
+          foi entender que consigo solucionar alguns dos problemas do mundo
+          através dos códigos e da tecnologia, tornando ele um lugar melhor. Sou
+          movido pela curiosidade e constantemente busco me aprofundar nesse
+          universo, sempre em busca de aprender mais e me atualizar sobre as
+          últimas tendências e avanços na área pelo qual tenho muito carinho.
+          Afinal, provavelmente serei uma das pessoas mais curiosas que você
+          pode encontrar.
         </p>
         {/*
         <button className="border-4 border-cyan-400 w-max text-2xl text-gray-100 rounded-lg transition hover:shadow-button hover:shadow-cyan-200 hover:-translate-y-2 shadow-xl shadow-black">
@@ -44,7 +45,8 @@ export function AboutMe() {
           loop
           mode="normal"
           src="https://assets7.lottiefiles.com/private_files/lf30_WdTEui.json"
-          style={{ width: '450px' }}
+          // eslint-disable-next-line prettier/prettier
+          style={{ width: "450px" }}
         ></lottie-player>
       </div>
     </main>
